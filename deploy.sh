86c5101eb2ad5b046ea138f26b30e2e059f88e84
Chinmay Jog <chinmaymjog@gmail.com>
2025-01-16 13:16:08 +0000

    restructure

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker rm -f  dev-site
-docker run -d -p 80:80 -p 443:443 --name dev-site -e WORDPRESS_DB_HOST=$DB_HOST:$DB_PORT -e WORDPRESS_DB_NAME=$DEV_DB -e WORDPRESS_DB_USER=$DEV_DB_USER -e WORDPRESS_DB_PASSWORD=$DEV_DB_PASSWORD $DOCKER_REPOSITORY:latest
